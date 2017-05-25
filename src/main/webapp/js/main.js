ad5675ec91cbd5a5f45194e1d5ef529341e9ef68
Dmitry <dimysik@gmail.com>
2017-05-26 01:03:32 +0300

    Ajax+jQuery

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,133 +1,137 @@
-$(document).ready(function () {
+var path = null;
+var separator = "";
+var parentPath = "";
 
-    var path = null;
-    var separator = "";
-    var parentPath = "";
-
+function queryServer(path) {
     $.ajax({
         type: "get",
         url: "directory",
         data: {path: path},
         success: function (result) {
-            update(result);
-            $("table").after(addUpload(result));
-
+            // $("#fmanager_panel > tr").remove();
+            $("table").find("tr:gt(0)").remove();
+            fillTable(result);
         }
     });
+}
 
-    function update(result) {
-        if (result.path != null) {
-            path = result.path;
-            separator = result.separator;
-            parentPath = result.parentPath;
+function fillTable(result) {
+    if (result.path !== null) {
+        path = result.path;
+        separator = result.separator;
+        parentPath = result.parentPath;
 
 
-            $("caption").text("Current directory: " + path);
-            $("table").append(appendParentRow(result));
+        $("caption").text("Current directory: " + path);
+        $("table").append(appendParentRow(result));
 
-            var contents = result.contents;
-            if (contents) {
-                $.each(contents, function (i, file) {
-                    $("table").append(appendFileRow(file));
-                });
-            }
+        var contents = result.contents;
+        if (contents) {
+            $.each(contents, function (i, file) {
+                $("table").append(appendFileRow(file));
+            });
         }
-    }
 
-    function appendParentRow(result) {
-        var isChild = result.isChild;
-        if (isChild === true) {
-            var rootCell = $("<a></a>").attr("href", "/directory?path=" + result.parentPath);
-            rootCell.attr("class", "parentPath");
-            var $href = rootCell.text("..");
-            var col2 = addTdTag("col_name").append($href);
-            return $("<tr></tr>").append(addTdTag("col_type"), col2, addTdTag("col_size"), addTdTag("col_download"));
-        }
-        return "";
+        // $("table").after(addUpload(result));
     }
+}
 
-    function addUpload(result) {
-        var $form = "";
-        var $inputFile = $("<input>").attr({
-            type: 'file',
-            name: "file",
-            id: "file"
-        });
-        var $inputPath = $('<input>').attr({
+function appendParentRow(result) {
+    var isChild = result.isChild;
+    if (isChild === "true") {
+        var rootCell = $("<a></a>").attr("href", "/directory?path=" + result.parentPath);
+        rootCell.attr("class", "parentPath");
+        var $href = rootCell.text("..");
+        var col2 = addTdTag("col_name");
+        col2.append($href);
+        return $("<tr></tr>").append(addTdTag("col_type"), col2, addTdTag("col_size"), addTdTag("col_download"));
+    }
+    return "";
+}
+
+function addTdTag(attributeName) {
+    return $("<td></td>").attr("class", attributeName);
+}
+
+function addUpload(result) {
+    var $form = "";
+    var $inputFile = $("<input>").attr({
+        type: 'file',
+        name: "file",
+        id: "file"
+    });
+    var $inputPath = $('<input>').attr({
+        type: 'hidden',
+        name: "path",
+        value: path
+    });
+    var $inputUpload = $('<input>').attr({
+        type: 'submit',
+        name: "upload",
+        value: "Upload",
+        id: "upload"
+    });
+    $form = $("<form></form>").attr("method", "POST")
+        .attr("action", "/upload")
+        .attr("enctype", "multipart/form-data");
+    return $form.append($inputFile, $inputPath, $inputUpload);
+}
+
+function appendFileRow(file) {
+    var isDirectory = file.isDirectory;
+    var td_col1 = $("<td></td>").attr("class", "col_type");
+    var col1 = td_col1.text(isDirectory ? "-" : "+");
+
+    var $fileName = "";
+    if (isDirectory) {
+        var filePath = file.relativePath + separator + file.name;
+        $fileName = $("<p></p>")//.attr("href", "/directory?path=" + filePath)
+            .attr("title", file.relativePath + separator + file.name)
+            .attr("class", "childPath")
+            .attr("onclick", "queryServer(\"" + filePath + "\")")
+            .text(file.name);
+    }
+    if (file.isFile) {
+        $fileName = $("<p></p>").attr("id", "childPath").text(file.name);
+    }
+    var $td_col2 = $("<td></td>").attr("class", "col_name");
+    var col2 = $td_col2.append($fileName);
+
+
+    var col3 = $("<td></td>").attr("class", "col_size").text(file.size);
+
+    var $form = "";
+    if (file.isFile) {
+        var $inputPath = $("<input>").attr({
             type: 'hidden',
             name: "path",
             value: path
         });
-        var $inputUpload = $('<input>').attr({
+        var $inputName = $('<input>').attr({
+            type: 'hidden',
+            name: "fileName",
+            value: file.name
+        });
+        var $inputButton = $('<input>').attr({
             type: 'submit',
-            name: "upload",
-            value: "Upload",
-            id: "upload"
+            name: "download",
+            value: "Download",
+            id: "download"
         });
         $form = $("<form></form>").attr("method", "POST")
-            .attr("action", "/upload")
-            .attr("enctype", "multipart/form-data");
-        return $form.append($inputFile, $inputPath, $inputUpload);
+            .attr("action", "/download")
+            .attr("class", "download");
+        $form.append($inputPath, $inputName, $inputButton);
     }
+    var $td_col4 = $("<td></td>").attr("class", "col_download");
+    var col4 = $td_col4.append($form);
 
-    function addTdTag(attributeName) {
-        return $("<td></td>").attr("class", attributeName);
-    }
+    var $tr = $("<tr></tr>");
+    return $tr.append(col1, col2, col3, col4);
+}
 
-    function appendFileRow(file) {
-        var isDirectory = file.isDirectory;
-        var td_col1 = $("<td></td>").attr("class", "col_type");
-        var col1 = td_col1.text(isDirectory ? "-" : "+");
-
-        var $fileName = "";
-        if (isDirectory) {
-            $fileName = $("<a></a>").attr("href", "/directory?path=" + file.relativePath + separator + file.name)
-                .attr("title", file.relativePath + separator + file.name)
-                .attr("class", "childPath")
-                .text(file.name);
-        }
-        if (file.isFile) {
-            $fileName = $("<p></p>").attr("id", "childPath").text(file.name);
-        }
-        var $td_col2 = $("<td></td>").attr("class", "col_name");
-        var col2 = $td_col2.append($fileName);
-
-
-        var col3 = $("<td></td>").attr("class", "col_size").text(file.size);
-
-        var $form = "";
-        if (file.isFile) {
-            var $inputPath = $("<input>").attr({
-                type: 'hidden',
-                name: "path",
-                value: path
-            });
-            var $inputName = $('<input>').attr({
-                type: 'hidden',
-                name: "fileName",
-                value: file.name
-            });
-            var $inputButton = $('<input>').attr({
-                type: 'submit',
-                name: "download",
-                value: "Download",
-                id: "download"
-            });
-            $form = $("<form></form>").attr("method", "POST")
-                .attr("action", "/download")
-                .attr("class", "download");
-            $form.append($inputPath, $inputName, $inputButton);
-        }
-        var $td_col4 = $("<td></td>").attr("class", "col_download");
-        var col4 = $td_col4.append($form);
-
-        var $tr = $("<tr></tr>");
-        return $tr.append(col1, col2, col3, col4);
-    }
-
-    $("#childPath").click(function () {
-        $(this).hide();
-    });
+$(document).ready(function () {
+    queryServer(null);
 });
 
 //$(document).ready(function () {
@@ -142,7 +146,7 @@ $(document).ready(function () {
 //        type: "get",
 //        url: uri,
 //        success: function (result) {
-//            update(result);
+//            fillTable(result);
 //        }
 //    });
 //});
@@ -153,7 +157,7 @@ $(document).ready(function () {
 //        type: "get",
 //        url: uri,
 //        success: function (result) {
-//            update(result);
+//            fillTable(result);
 //        }
 //    });
 //});
@@ -165,7 +169,7 @@ $(document).ready(function () {
 //        url: uri,
 //        data: {path: parentPath + separator + $(this).text()},
 //        success: function (result) {
-//            update(result);
+//            fillTable(result);
 //        }
 //    });
 //});
