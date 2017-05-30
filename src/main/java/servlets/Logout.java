f98423f482c1cdb11dea8977058df9f0a7ae5c01
Dmitry <dimysik@gmail.com>
2017-05-31 00:58:42 +0300

    Authorization

--- src/main/java/servlets/Logout.java
+++ src/main/java/servlets/Logout.java
@@ -15,6 +15,8 @@ public class Logout extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/WEB-INF/views/logout.xhtml").include(request, response);
+        HttpSession session = request.getSession();
+        session.invalidate();
     }
 
     @Override
